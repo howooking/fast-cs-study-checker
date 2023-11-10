bcbab4dc9a9bd5b62e5e6f766bea1f93e5444f4c
howooking <junsgk@gmail.com>
2023-11-10 23:28:39 +0900

    홈페이지

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default async function Home() {
-  return <>helloworld</>;
+"use client";
+
+import Lottie from "lottie-react";
+import lotti from "@/public/animation_check.json";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center gap-2 p-2 mt-10">
+      <h1 className="text-3xl">CS 스터디 체커</h1>
+      <Lottie animationData={lotti} className="w-[400px]" />
+      <p className="text-md sm:text-2xl">선 체크 필승</p>
+      <Link href="/subjects">
+        <Button>시작하기</Button>
+      </Link>
+    </div>
+  );
 }
